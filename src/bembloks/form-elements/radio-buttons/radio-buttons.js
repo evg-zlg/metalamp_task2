e2acc75e6eabab88c9bdeb217a3ee09ba85fe9a2
evg-zlg <evg.zalogin@gmail.com>
2021-12-24 11:33:42 +0500

    Added like buttons, styled, js

--- src/bembloks/form-elements/radio-buttons/radio-buttons.js
+++ src/bembloks/form-elements/radio-buttons/radio-buttons.js
@@ -2,4 +2,4 @@ document.querySelectorAll(".radio__input").forEach(function (dropDownWrapper) {
   dropDownWrapper.addEventListener("click", function() {
     dropDownWrapper.parentElement.classList.toggle("radio-button__label--checked");
   });
-})
+});
