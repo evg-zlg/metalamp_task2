653eee07da30de933c5619b1bd2e0b319a0ae216
evg-zlg <evg.zalogin@gmail.com>
2021-12-24 09:23:12 +0500

    Added toggle, styled

---
+++ src/bembloks/form-elements/radio-buttons/radio-buttons.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".radio__input").forEach(function (dropDownWrapper) {
+  dropDownWrapper.addEventListener("click", function() {
+    dropDownWrapper.parentElement.classList.toggle("radio-button__label--checked");
+  });
+})
